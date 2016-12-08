3bb85eeedb1b99b228fbada043ff6231b4fb19b1
Sherwood Callaway <shcallaway@gmail.com>
2016-12-08 08:54:12 -0500

    Reading from my sheet (not sample)

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # Cardician
-cardician (car • dish • un) | *noun* | A magician who manipulates playing cards.
+cardician (car • dish • un) *noun* | A magician who manipulates playing cards.
 
 Add professional connections to a Google sheet from CLI. 
 
 ## Resources
 * [Google Sheets API Ruby Quickstart](https://developers.google.com/sheets/quickstart/ruby)
+# [How to Write A1 Notation](https://msdn.microsoft.com/en-us/library/bb211395(v=office.12).aspx)
--- cardician.rb
+++ cardician.rb
@@ -45,14 +45,13 @@ service = Google::Apis::SheetsV4::SheetsService.new
 service.client_options.application_name = APPLICATION_NAME
 service.authorization = authorize
 
-# Prints the names and majors of students in a sample spreadsheet:
-# https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
-spreadsheet_id = '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms'
-range = 'Class Data!A2:E'
+# Read columns A through C from spreadsheet:
+# https://docs.google.com/spreadsheets/d/1tii5sC0lGTPOUteb7p9JxVqxK45RcCbOCCeEbgD5rv0/edit
+spreadsheet_id = '1tii5sC0lGTPOUteb7p9JxVqxK45RcCbOCCeEbgD5rv0'
+range = 'A:C'
 response = service.get_spreadsheet_values(spreadsheet_id, range)
-puts 'Name, Major:'
 puts 'No data found.' if response.values.empty?
 response.values.each do |row|
-  # Print columns A and E, which correspond to indices 0 and 4.
-  puts "#{row[0]}, #{row[4]}"
+  # Print columns A thrugh C
+  puts "#{row[0]}, #{row[1]}, #{row[2]}"
 end
