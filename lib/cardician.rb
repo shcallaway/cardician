6ac7d7c3a88fe43a5abe7b0e890ba8e84a24b5d1
Sherwood <shcallaway@gmail.com>
2016-12-08 22:29:47 -0500

    Trying to grab name from LinkedIn page

--- lib/cardician.rb
+++ lib/cardician.rb
@@ -1,25 +1,47 @@
 require "google_drive"
+require "httparty"
+require "nokogiri"
+require "pry"
 
-# Create a session. Prompts for credentials the first time.
-# Saves to config.json for later.
-session = GoogleDrive::Session.from_config("config.json")
+# Store the LinkedIn URL form command line.
+linkedin_url = "https://www.linkedin.com/in/sherwoodcallaway"
 
-# Grab the first worksheet of Google sheet: 1tii5sC0lGTPOUteb7p9JxVqxK45RcCbOCCeEbgD5rv0
-ws = session.spreadsheet_by_key("1tii5sC0lGTPOUteb7p9JxVqxK45RcCbOCCeEbgD5rv0").worksheets[0]
+# Get the contents of the LinkedIn page.
+page = HTTParty.get(linkedin_url, follow_redirects: true)
 
-# Find the next available row.
-row = ws.num_rows + 1
+# Page is actually just an HTML header with a JS redirect -- need to get to the final page somehow?
+p page.parsed_response
 
-# Populate cells in new row.
-# Need to switch this out for command line args.
-ws[row, 1] = "Jane"
-ws[row, 2] = "Doe"
+# Once I get to the final page, I can do all this:
 
-# Save the changes to the server.
-ws.save()
+# # Parse the page with Nokogiri.
+# parse_page = Nokogiri::HTML(page)
 
-# Print the new entry.
-p "New Spreadsheet Entry: #{ws[row, 1]} #{ws[row, 2]}"
+# full_name = parse_page.css("span.full-name").text
 
-# Reload the worksheet to get changes by other clients.
-ws.reload
+# Parse the full_name into first_name and last_name.
+
+# # Create a session. Prompts for credentials the first time.
+# # Saves to config.json for later.
+# session = GoogleDrive::Session.from_config("config.json")
+
+# # Grab the first worksheet of Google sheet: 1tii5sC0lGTPOUteb7p9JxVqxK45RcCbOCCeEbgD5rv0
+# ws = session.spreadsheet_by_key("1tii5sC0lGTPOUteb7p9JxVqxK45RcCbOCCeEbgD5rv0").worksheets[0]
+
+# # Find the next available row.
+# row = ws.num_rows + 1
+
+# # Populate cells in new row.
+# # Need to switch this out for command line args.
+# ws[row, 1] = first_name
+# ws[row, 2] = last_name
+# ws[row, 3] = linkedin_url
+
+# # Save the changes to the server.
+# ws.save()
+
+# # Print the new entry.
+# p "New Spreadsheet Entry: #{ws[row, 1]} #{ws[row, 2]}"
+
+# # Reload the worksheet to get changes by other clients.
+# ws.reload
