a59c831b6ea1893f2374068dcaa3aabe994219e0
Sherwood <shcallaway@gmail.com>
2016-12-10 16:57:28 -0500

    Implemented saving of API access keys

--- lib/cardician.rb
+++ lib/cardician.rb
@@ -2,21 +2,31 @@ require 'google_drive'
 require 'linkedin'
 require 'dotenv'
 
+# Abort if no args supplied.
+if ARGV.empty?
+  abort("ERROR: LinkedIn URL missing.")
+end
+
 # Get the user's LinkedIn URL from args.
 URL = ARGV[0]
 ARGV.clear
 
 # Load the LinkedIn API credentials.
-Dotenv.load
-ID = ENV['LINKEDIN_ID']
-SECRET = ENV['LINKEDIN_SECRET']
+if File.exists?(".env")
+  Dotenv.load
+  ID = ENV['LINKEDIN_ID']
+  SECRET = ENV['LINKEDIN_SECRET']
+else 
+    abort("ERROR: LinkedIn API credentials missing.")
+end
 
 # Set up the LinkedIn client.
 client = LinkedIn::Client.new(ID, SECRET)
 
 # Look for some existing access keys.
 if File.exists?("linkedin.txt")
-  access_keys = File.open("linkedin.txt", &:gets).split(",")
+  # Read the string from file and convert to array.
+  access_keys = File.open("linkedin.txt", &:gets).gsub(/[\[\]"]/, "").split(", ")
 else
   access_keys = false
 end 
@@ -35,7 +45,7 @@ else
   pin_url = request_token.authorize_url
 
   # Direct the user to authenticate manually via browser and enter the pin.
-  puts "ACTION REQUIRED: You need to authenticate this app with LinkedIn. "\
+  puts "You need to authenticate this app with LinkedIn. "\
        "To authorize, visit this URL:\n#{pin_url}\n"
   print "Copy the five digit code and paste it here:"
 
@@ -52,21 +62,35 @@ else
     end
   rescue
     # Abort if the authorization fails. 
-    abort("Invalid authorization code. Try reshuffling.")
+    abort("ERROR: Invalid authorization code.")
   end 
 end
 
+# Try to find the LinkedIn user.
+begin
+  user = client.profile(url: URL)
+rescue
+  abort("ERROR: Can't find a profile with that URL.")
+end
+
 # Get relevant info from the LinkedIn user.
-user = client.profile(url: URL)
 first_name = user.first_name
 last_name = user.last_name
 
 # Create a new Google session. 
 # (Prompts for credentials the first time. Saves to config.json after.)
-session = GoogleDrive::Session.from_config("google.json")
+begin
+  session = GoogleDrive::Session.from_config("google.json")
+rescue
+  abort("ERROR: Google authorizaiton failed.")
+end
 
 # Grab the first worksheet of Google sheet: 1tii5sC0lGTPOUteb7p9JxVqxK45RcCbOCCeEbgD5rv0
-ws = session.spreadsheet_by_key("1tii5sC0lGTPOUteb7p9JxVqxK45RcCbOCCeEbgD5rv0").worksheets[0]
+begin
+  ws = session.spreadsheet_by_key("1tii5sC0lGTPOUteb7p9JxVqxK45RcCbOCCeEbgD5rv0").worksheets[0]
+rescue
+  abort("ERROR: Invalid spreadsheet ID.")
+end
 
 # Find the next available row.
 row = ws.num_rows + 1
@@ -77,7 +101,11 @@ ws[row, 2] = last_name
 ws[row, 3] = URL
 
 # Save the changes to the server.
-ws.save()
+begin
+  ws.save()
+rescue
+  abort("ERROR: Failed to save.")
+end
 
 # Print the success statement.
 puts "Success! A new card has been added to your deck."
